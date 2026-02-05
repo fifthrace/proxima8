1b2adce119c0254187684f2e0315fc09b9d57bc1
Atlas <atlas@clawd.bot>
2026-02-04 23:05:03 -0500

    feat: expand 8x8 category to 20 levels

--- scripts/gen_8x8.py
+++ scripts/gen_8x8.py
@@ -82,9 +82,9 @@ if __name__ == "__main__":
     # Filter out 10x10 and 15x15
     new_manifest = [l for l in manifest if l['width'] not in [10, 15]]
     
-    # Generate 4 new 8x8 levels
-    generated_count = 0
-    while generated_count < 4:
+    # Generate more 8x8 levels (up to 20 total)
+    generated_count = len([l for l in new_manifest if l['width'] == 8])
+    while generated_count < 20:
         l = generate_level(8, 8)
         if l:
             # Check for duplicates in manifest
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'proxima8-v9';
+const CACHE_NAME = 'proxima8-v10';
 const ASSETS = [
   './',
   './index.html',
